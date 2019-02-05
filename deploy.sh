b8a027d6f80f4bb1e175a1273a2120b27296db5a
Danny Lin <danny@kdrag0n.dev>
2019-02-05 14:59:33 -0800

    deploy: Delete 2nd-stage script after execution
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 GOOS=linux GOARCH=arm64 go build -ldflags="-s -w" && adb push kbench deploy.stage2.sh /data/local/tmp > /dev/null && adb shell chmod 755 /data/local/tmp/deploy.stage2.sh && adb shell su -c /data/local/tmp/deploy.stage2.sh "$@"
+adb shell rm -f /data/local/tmp/deploy.stage2.sh
 
