d3b13b17bcd1656ba612734504da59f0c3d42a0c
Danny Lin <danny@kdrag0n.dev>
2019-02-05 14:59:04 -0800

    test: Rename to deploy
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+GOOS=linux GOARCH=arm64 go build -ldflags="-s -w" && adb push kbench deploy.stage2.sh /data/local/tmp > /dev/null && adb shell chmod 755 /data/local/tmp/deploy.stage2.sh && adb shell su -c /data/local/tmp/deploy.stage2.sh "$@"
+
