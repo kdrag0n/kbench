aec2aa8ced8f202a57079da3bae76b5c7e646123
Danny Lin <danny@kdrag0n.dev>
2020-05-01 10:28:41 -0700

    bench: Add more space for scores

--- bench.go
+++ bench.go
@@ -133,7 +133,7 @@ func runBenchmarks(trials int, speed Speed, monitorPower bool, powerInterval uin
 			check(err)
 
 			spaces := strings.Repeat(" ", maxBmNameLen-len(bm.Name))
-			fmt.Printf("%s%4.0f  (%9.1f %4s; runtime: %3s)\n", spaces, benchScore, rawValue, bm.Unit, formatDuration(duration))
+			fmt.Printf("%s%6.0f  (%9.1f %4s; runtime: %3s)\n", spaces, benchScore, rawValue, bm.Unit, formatDuration(duration))
 
 			// Multiply the geometric mean counter: ts = ts * bs
 			_, err = decCtx.Mul(trialCnt, trialCnt, floatToDec(benchScore))
