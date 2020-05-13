41586d964e1eb3492a1900ad0cce8c67bc9f7faf
Danny Lin <danny@kdrag0n.dev>
2020-05-13 15:43:16 -0700

    kernel: Remove getTickRate
    This is no longer used and it has never been reliable — it returns
    USER_HZ, which is always 100 Hz.

--- kernel.go
+++ kernel.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"io/ioutil"
-	"strconv"
 	"strings"
 )
 
@@ -14,18 +13,4 @@ func readFileLine(path string) (cleaned string, err error) {
 
 	cleaned = strings.TrimSuffix(string(raw), "\n")
 	return
-}
-
-func getTickRate() float64 {
-	uptimeStr, err := readFileLine("/proc/uptime")
-	check(err)
-	uptimeSec, err := strconv.ParseFloat(strings.Fields(uptimeStr)[0], 64)
-	check(err)
-
-	statStr, err := readFileLine("/proc/self/stat")
-	check(err)
-	statTimeoutJiffies, err := strconv.Atoi(strings.Fields(statStr)[21])
-	check(err)
-
-	return uptimeSec / float64(statTimeoutJiffies)
 }
